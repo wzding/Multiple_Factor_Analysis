c4f682a68e7b9b8443de6f155458bc9a1b720338
wzding <wzding6706@gmail.com>
2016-11-29 14:41:54 -0800

    add install file for emma

---
+++ final/MFA01/install_Emma.R
@@ -0,0 +1,19 @@
+
+setwd('C:/Users/Emma/Downloads/stats-243/final/')
+
+library(devtools)
+
+pkg <- 'MFA01'
+
+devtools::document(pkg = pkg)
+devtools::check_man(pkg = pkg)
+devtools::test(pkg = pkg)
+devtools::build_vignettes(pkg = pkg)
+devtools::build(pkg = pkg)
+devtools::install(pkg = pkg)
+
+library(MFA01)
+
+# a <- MFA()
+# a
+
