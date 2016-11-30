00ea04b56754d405ae9a3fc6336168c490882062
wzding <wzding6706@gmail.com>
2016-11-29 16:50:48 -0800

    change text

--- final/ui.R
+++ final/ui.R
@@ -1,5 +1,7 @@
 library(shiny)
 
+x <- paste(cat("Which table to analyze", "\n","when selecting partial factor score or factor loading"))
+
 # Define UI for application that graphs relative freqs
 shinyUI(fluidPage(
 
@@ -14,14 +16,15 @@ shinyUI(fluidPage(
                               "Partial Factor Score","Factor Loading",
                               "Partial Factor Score and Factor Loading"),
                   selected = "Common Factor Score"),
-      sliderInput("tester","Note: only useful when selecting partial
-                  factor score or factor loading",min=1,max=10,value=1)
+      sliderInput("tester",paste("Which table to analyze for partial factor score or factor loading"),
+                  min=1,max=10,value=1)
 
     ),
 
     # Show a plot of the generated distribution
     mainPanel(
       textOutput("text"),
+      textOutput("text_sub"),
       plotOutput("result_plot"),
       textOutput("text_table"),
       #tableOutput("eigTbl")
