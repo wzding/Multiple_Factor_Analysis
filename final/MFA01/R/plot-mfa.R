d53058eb78b66298c3ac26706ebaa873bb49926c
wzding <wzding6706@gmail.com>
2016-11-29 16:15:07 -0800

    change plot color

--- final/MFA01/R/plot-mfa.R
+++ final/MFA01/R/plot-mfa.R
@@ -20,15 +20,11 @@ plot.mfa <- function(x, cfs = TRUE, pfs = FALSE, pfl = FALSE, num=1,eig=FALSE,..
   plot(x$cfs[,1:2],xaxt="n", yaxt="n",
        xlab="1st component",
        ylab="2nd component",
-       bty="n",cex=1.5,pch = c(rep(15,4), rep(16,4), rep(17,4)),
-       col = c(rep("red",4), rep("green",4),rep("blue",4)))
+       bty="n",cex=1.5,pch = 19,col = "red")
   axis(side=1,pos = 0)
   axis(side=2,pos = 0)
   abline(v=0,h=0,col = "gray70", lwd = 1.5)
-  # text(GSVD.factor.score[,1],GSVD.factor.score[,2],labels = label ,cex=0.8,pos=3)
   title(main="Common Facor scores")
-  # legend("bottomright",pch = c(15, 16, 17),col = c("red", "green","blue"),
-  #         c("New Zealand","France","Canada"))
 
   if(pfs && pfl){
     pfs_scale <- rescale(x$pfs,sqrt(x$eigen))
@@ -41,7 +37,8 @@ plot.mfa <- function(x, cfs = TRUE, pfs = FALSE, pfl = FALSE, num=1,eig=FALSE,..
   }
   else if(pfs){
     par(mar=c(1,1,1,1))
-    plot(x$pfs[[num]][,1:2],xaxt="n", yaxt="n",xlab="1st component",ylab="2nd component",bty="n",cex=1.5)
+    plot(x$pfs[[num]][,1:2],xaxt="n", yaxt="n",xlab="1st component",ylab="2nd component",
+         bty="n",cex=1.5,pch = 17,col = "red")
     axis(side=1,pos = 0)
     axis(side=2,pos = 0)
     abline(v=0,h=0)
@@ -50,7 +47,8 @@ plot.mfa <- function(x, cfs = TRUE, pfs = FALSE, pfl = FALSE, num=1,eig=FALSE,..
   }
   else if(pfl){
     par(mar=c(1,1,1,1))
-    plot(x$pfl[[num]][,1:2],xaxt="n", yaxt="n",xlab="1st component",ylab="2nd component",bty="n",cex=1.5)
+    plot(x$pfl[[num]][,1:2],xaxt="n", yaxt="n",xlab="1st component",ylab="2nd component",
+         bty="n",cex=1.5,pch = 15,col = "red")
     axis(side=1,pos = 0)
     axis(side=2,pos = 0)
     abline(v=0,h=0)
