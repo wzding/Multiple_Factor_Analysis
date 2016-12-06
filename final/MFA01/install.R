d7e2d48a0c1397c6b0dca13f811ece3b98e2a603
wzding <wzding6706@gmail.com>
2016-12-06 12:53:45 -0800

    package test

--- final/MFA01/install.R
+++ final/MFA01/install.R
@@ -21,11 +21,11 @@ library(MFA01)
 
 
 # --------------------below is function testing
-# a <- MFA()
-# a
-# 
-# a <- MFA(ncomps = 2)
-# a
+a <- MFA()
+a
+
+a <- MFA(ncomps = 2)
+a
 # x <- matrix(1:100, nrow=10, ncol=10)
 # s1 <- list(1:3,5:6,7:10)
 # MFA(x,s1)
